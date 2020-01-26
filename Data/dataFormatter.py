5277e727d2a34ab0ab75870804e534d18b1abceb
Sebastian Engel <contact@sebastianengel.eu>
2020-01-26 23:31:23 +0100

    data files added

---
+++ Data/dataFormatter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+import math
+
+# This script converts the OPAL++ optimization results archive (Gen.csv) to a
+# more readable format
+
+# Raw Optimization Archives
+par3source = "Gen_Opt1.csv"
+par5source = "Gen_Opt2.csv"
+
+headers5par = ["ID", "Validity", "constrainViolation", "efficiency", "hemolysis", "dsp", "rdin", "rdout", "volconstr", "volsp", "bladenum", "dskth", "diffangle", "diffratio", "tonguerd", "initialspeed", "icemversion", "speed", "presdrop", "torque", "effrep", "hemorep" ]
+headers3par = ["ID", "Validity", "constrainViolation", "efficiency", "hemolysis", "dsp", "volconstr", "volsp", "rdout", "rdin", "bladenum", "dskth", "diffangle", "diffratio", "tonguerd", "initialspeed", "icemversion", "speed", "presdrop", "torque", "effrep", "hemorep" ]
+
+
+
+
+
+#gen=pd.read_csv(par5source ,sep=' ',header=None, skiprows=[0])
+par5=pd.read_csv(par5source ,sep=' ',header=None)
+par5.columns = headers5par
+par3=pd.read_csv(par3source ,sep=' ',header=None)
+par3.columns = headers3par
+
+
+par5=par5.drop(columns=["ID", "constrainViolation", "bladenum", "dskth", "diffangle", "diffratio", "tonguerd", "initialspeed", "icemversion", "speed", "presdrop", "torque", "effrep", "hemorep"])
+par3=par3.drop(columns=["ID", "constrainViolation", "bladenum", "dskth", "diffangle", "diffratio", "tonguerd", "initialspeed", "icemversion", "speed", "presdrop", "torque", "effrep", "hemorep"])
+
+par3=par3.rename(columns={"efficiency":"hydralic_efficiency(-)", "hemolysis":"Hemolysis_ratio(-)", "dsp":"gap_width(mm)", "rdin":"inner_disk_radius(mm)", "rdout":"outer_disk_radius(mm)", "volconstr":"volute_constructor_parameter(-)"})
+par5=par5.rename(columns={"efficiency":"hydralic_efficiency(-)", "hemolysis":"Hemolysis_ratio(-)", "dsp":"gap_width(mm)", "rdin":"inner_disk_radius(mm)", "rdout":"outer_disk_radius(mm)", "volconstr":"volute_constructor_parameter(-)"})
+
+par3.to_csv("Opt1.csv")
+par5.to_csv("Opt2.csv")
